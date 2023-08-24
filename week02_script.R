a79394fbdfe1cbfea50438ed0b5be6897ba71978
ZacharyBunch <zachbunch2002@gmail.com>
2023-08-24 10:43:03 -0400

    Tidyverse and more

--- week02_script.R
+++ week02_script.R
@@ -305,3 +305,126 @@ mean_temperature_NC
 
 mean_abundance_VA
 mean_abundance_NC
+
+
+# Tidyverse ---------------------------------------------------------------
+
+library(tidyverse)
+
+iris <- as_tibble(iris)
+iris
+
+# single match "=="
+filter(iris, Species == "virginica")
+
+# multiple match "%in%"
+filter(iris, Species %in% c("virginica", "versicolor"))
+
+# except "!="
+filter(iris, Species != "virginica")
+
+# except multiple "!(x %in% c("a", "b"))
+filter(iris, !(Species %in% c("virginica", "versicolor")))
+
+# greater than ">"
+filter(iris, Sepal.Length > 5)
+
+# equal & greater than ">="
+filter(iris, Sepal.Length >= 5)
+
+# less than "<"
+filter(iris, Sepal.Length < 5)
+
+# equal & less than "<="
+filter(iris, Sepal.Length <= 5)
+
+
+# Arrange -----------------------------------------------------------------
+
+# arrange in an ascending order
+arrange(iris, Sepal.Length)
+
+# arrange in an descending order
+arrange(iris, desc(Sepal.Length))
+
+
+# Select ------------------------------------------------------------------
+
+# select one column
+select(iris, Sepal.Length)
+
+# select multiple columns
+select(iris, c(Sepal.Length, Sepal.Width))
+
+# remove one column
+select(iris, -Sepal.Length)
+
+# remove multiple columns
+select(iris, -c(Sepal.Length, Sepal.Width))
+
+# select/remove multiple columns with a start rule
+# starts_with("x")
+select(iris, starts_with("Sepal"))
+select(iris, -starts_with("Sepal"))
+
+# select/remove multiple columns with an end rule
+# ends_with("x")
+select(iris, ends_with("Width"))
+select(iris, -ends_with("Width"))
+
+
+# Mutate ------------------------------------------------------------------
+
+# add a new column
+x <- 1:150
+mutate(iris, x = x)
+mutate(iris, SL2 = 2 * Sepal.Length)
+
+
+# Piping ------------------------------------------------------------------
+
+# the following codes produce the same data frame
+# apply functions separately
+df_vir <- filter(iris, Species == "virginica")
+df_vir_sl <- select(df_vir, Sepal.Length)
+print(df_vir_sl)
+
+# piping
+iris %>% 
+  filter(Species == "virginica") %>% 
+  select(Sepal.Length)
+
+
+# group operation ---------------------------------------------------------
+
+# grouping by "Species", then take means "Speal.Length" for each species
+iris %>% 
+  group_by(Species) %>% 
+  summarize(mu_sl = mean(Sepal.Length))
+
+# grouping by "Species", then take means & SD "Speal.Length" for each species
+iris %>% 
+  group_by(Species) %>% 
+  summarize(mu_sl = mean(Sepal.Length),
+            sd_sl = sd(Sepal.Length))
+
+# grouping by "Species", then take means "Speal.Length" for each species
+iris %>% 
+  group_by(Species) %>% 
+  mutate(mu_sl = mean(Sepal.Length)) %>% 
+  ungroup()
+
+
+# Left join ---------------------------------------------------------------
+
+# matching by a single column
+## left join by "Species": one to one
+df1 <- tibble(Species = c("A", "B", "C"),
+              x = c(1, 2, 3))
+
+df2 <- tibble(Species = c("A", "B", "C"),
+              y = c(4, 5, 6))
+
+left_join(x = df1,
+          y = df2,
+          by = "Species")
