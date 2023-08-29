b9e88a73f61a097a52db4c119c179ac2225bb77b
ZacharyBunch <zachbunch2002@gmail.com>
2023-08-29 10:23:59 -0400

    facet wrap ex.

--- week03_script.R
+++ week03_script.R
@@ -34,3 +34,57 @@ df0 %>%
              y = y)) +
   geom_line()
 
+# basic plot; bins = 30 by default
+iris %>% 
+  ggplot(aes(x = Sepal.Length)) +
+  geom_histogram()
+
+# change bin width
+iris %>% 
+  ggplot(aes(x = Sepal.Length)) +
+  geom_histogram(binwidth = 0.5)
+
+# change bin number
+iris %>% 
+  ggplot(aes(x = Sepal.Length)) +
+  geom_histogram(bins = 50)
+
+# basic plot
+iris %>% 
+  ggplot(aes(x = Species,
+             y = Sepal.Length)) +
+  geom_boxplot()
+
+# change fill by "Species"
+iris %>% 
+  ggplot(aes(x = Species,
+             y = Sepal.Length,
+             fill = Species)) +
+  geom_boxplot()
+
+# change fill by "Species", but consistent color
+iris %>% 
+  ggplot(aes(x = Species,
+             y = Sepal.Length,
+             fill = Species)) +
+  geom_boxplot(color = "darkgrey")
+
+# density plot
+
+iris %>% 
+  ggplot(aes(x= Sepal.Length)) +
+  geom_density(fill = "salmon", color = "salmon", alpha = 0.5)
+
+
+#facet: facet_wrap
+
+iris %>% 
+  ggplot(aes(x= Sepal.Length, y = Sepal.Width)) +
+  geom_point() +
+  facet_wrap(facets = ~ Species, nrow = 2, ncol = 2)
+
+iris %>% 
+  mutate(site = sample(letters[1:2], nrow(.), replace = TRUE )) %>% 
+  ggplot(aes(x= Sepal.Length, y = Sepal.Width)) +
+  geom_point() +
+  facet_grid(rows = vars(Species), cols = vars(site))
