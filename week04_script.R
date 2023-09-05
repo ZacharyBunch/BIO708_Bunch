6985bd8438bb42048180c725bed61eb0ac6c4735
ZacharyBunch <zachbunch2002@gmail.com>
2023-09-05 10:01:00 -0400

    Big push

--- week04_script.R
+++ week04_script.R
@@ -5,4 +5,53 @@
 #' author: Zachary Bunch
 #' ---
 
+library(tidyverse)
 
+# construct vectors x and y
+x <- c(15.9, 15.1, 21.9, 13.3, 24.4)
+y <- c(15.9, 15.1, 21.9, 53.3, 24.4)
+
+x
+
+y
+
+# arithmetic mean
+# for vector x
+n_x <- length(x) # the number of elements in x = the number of data points
+sum_x <- sum(x) # summation for x
+mu_x <- sum_x / n_x # arithmetic mean
+print(mu_x) # print calculated value
+
+
+# for vector y; we can calculate directly too
+mu_y <- sum(y) / length(y)
+print(mu_y) # print calculated value
+
+#mean()
+print(mean(x))
+print(mean(y))
+
+#Geometric mean ####
+
+# for vector x
+prod_x <- prod(x) # product of vector x; x1 * x2 * x3...
+n_x <- length(x)
+mug_x <- prod_x^(1 / n_x) # ^ means power
+print(mug_x)
+
+# for vector y
+mug_y <- prod(y)^(1 / length(y))
+print(mug_y)
+
+#Median ####
+
+# for vector x
+x <- sort(x) # sort x from small to large
+index <- (length(x) + 1) / 2 # (N + 1)/2 th index as length(x) is an odd number
+med_x <- x[index]
+print(med_x)
+
+# for vector y
+y <- sort(y) # sort y from small to large
+med_y <- y[(length(y) + 1) / 2]
+print(med_y)
