902b79e3f4ec50f55357aa6bcaa53d618bc898d0
ZacharyBunch <zachbunch2002@gmail.com>
2023-09-12 10:03:58 -0400

    9/12 class examples

---
+++ week05_script.R
@@ -0,0 +1,38 @@
+#' ---
+#' title: "Report week4"
+#' output: html_document
+#' date: "2023-9-23"
+#' author: Zachary Bunch
+#' ---
+
+
+# in class ----------------------------------------------------------------
+library(tidyverse)
+
+#plant data frame
+
+h <- c(16.9, 20.9, 15.8, 28, 21.6, 15.9, 22.4, 23.7, 22.9, 18.5)
+
+df_h1 <- tibble(plant_id = 1:10, # a vector from 1 to 10 by 1
+                height = h, # height
+                unit = "cm") # unit
+# nrow() returns the number of rows
+# while piping, "." refers to the dataframe inherited 
+# i.e., nrow(.) counts the number of rows in df_h1
+df_h1 <- df_h1 %>% 
+  mutate(mu_height = mean(height),
+         var_height = sum((height - mu_height)^2) / nrow(.))
+
+print(df_h1)
+
+#another set
+
+h <- c(27.6, 21.9, 16.9, 8.9, 25.6, 19.8, 19.9, 24.7, 24.1, 23)
+
+df_h2 <- tibble(plant_id = 11:20, # a vector from 11 to 20 by 1
+                height = h,
+                unit = "cm") %>% 
+  mutate(mu_height = mean(height),
+         var_height = sum((height - mu_height)^2) / nrow(.))
+
+print(df_h2)
