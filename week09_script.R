93305312b23be31d438dc2645cb1838c5cfabc5d
ZacharyBunch <zachbunch2002@gmail.com>
2023-10-17 09:53:50 -0400

    Lab done :D

--- week09_script.R
+++ week09_script.R
@@ -100,3 +100,41 @@ df_algae %>%
                    yend = biomass - eps), # end-coord y
                linetype = "dashed")
 
+
+# Lab  --------------------------------------------------------------------
+
+library(tidyverse)
+
+head(iris)
+
+setosa <- iris %>% filter(Species == "setosa")
+versicolor <- iris %>% filter(Species == "versicolor")
+virginica <- iris %>% filter(Species == "virginica")
+
+#Petal.Width
+
+setosa_reg <- lm(Sepal.Width ~ Petal.Width,
+        data = setosa)
+summary(setosa_reg)
+
+versicolor_reg <- lm(Sepal.Width ~ Petal.Width,
+                 data = versicolor)
+summary(versicolor_reg)
+
+virginica_reg <- lm(Sepal.Width ~ Petal.Width,
+                     data = virginica)
+summary(virginica_reg)
+
+#Petal.Length
+
+setosa_reg2 <- lm(Sepal.Width ~ Petal.Length + Petal.Width,
+                 data = setosa)
+summary(setosa_reg2)
+
+versicolor_reg2 <- lm(Sepal.Width ~ Petal.Length + Petal.Width,
+                     data = versicolor)
+summary(versicolor_reg2)
+
+virginica_reg2 <- lm(Sepal.Width ~ Petal.Length + Petal.Width,
+                    data = virginica)
+summary(virginica_reg2)
