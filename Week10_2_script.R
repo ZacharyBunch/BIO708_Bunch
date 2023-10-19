08f383b629bf018ca7bc2ed2cf94585d25db1193
ZacharyBunch <zachbunch2002@gmail.com>
2023-10-19 10:30:15 -0400

    Update

--- Week10_2_script.R
+++ Week10_2_script.R
@@ -10,7 +10,7 @@ library(dbplyr)
 
 # read data
 
-df_fl <- read_csv(here:here("data_raw/data_fish_length.csv"))
+df_fl <- read_csv(here::here("data_raw/data_fish_length.csv"))
 
 m <- lm(length ~ lake, 
         data = df_fl)
@@ -35,7 +35,7 @@ mu_b <- df_fl %>%
 
 # difference beten two groups
 
-b <- mu_b = mu_a
+b <- mu_b - mu_a
 
 
 
@@ -58,4 +58,40 @@ b <- df_fl %>%
 
 
 t.test(b,a, var.equal = true)
+
+#anova
+
+df_anova <- read_csv(here::here("data_Raw/data_fish_length_anova.csv"))
+
+lm(length ~ lake, 
+   data = df_anova)
+
+#get difference from lake a
+
+df_mu <- df_anova %>% 
+  group_by(lake) %>% 
+  summarize(mu = mean(length)) %>% 
+  arrange(lake) %>% 
+  pull(mu)
+
+# do anova with aov()
+
+m_aov <-aov(length ~ lake, data = df_anova)
+
+
+# compare the lm() and aov() output
+
+summary(m_aov)
+
+
+# combine different predictors --------------------------------------------
+
+iris <- as_tibble(iris)
+
+
+# develop iris model
+m_iris <- lm(Petal.Length ~ Petal.Width + Species,
+             data = iris)
+
+summary(m_iris)
 
