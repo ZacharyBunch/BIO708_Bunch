a7dec7efa686ef6ab5c96b60d4b3b9bf975342c0
ZacharyBunch <zachbunch2002@gmail.com>
2023-10-26 10:07:27 -0400

    summary update

--- Week11_script.R
+++ Week11_script.R
@@ -4,5 +4,49 @@
 #' date: "2023-10-12"
 #' author: Zachary Bunch
 #' ---
-#' 
-#' 
+
+
+# in class ----------------------------------------------------------------
+
+library(tidyverse)
+
+df_count <- read_csv("data_raw/data_garden_count.csv")
+print(df_count)
+
+#try linear model
+
+m_normal <- lm(count ~ nitrate,
+               df_count)
+
+summary(m_normal)
+
+
+## draw line
+
+a <- coef(m_normal)[1] # intercept
+b <- coef(m_normal)[2] # slope
+
+df_count %>%  ggplot(aes(y = count, x= nitrate)) + 
+  geom_point() +
+  geom_abline(intercept = a, slope = b)
+
+
+# extract estimates
+alpha <- coef(m_normal)[1] # intercept
+beta <- coef(m_normal)[2] # slope
+
+df_count %>% 
+  ggplot(aes(x = nitrate,
+             y = count)) +
+  geom_point() +
+  geom_abline(intercept = alpha,
+              slope = beta)
+
+## try GLM
+
+m_pois <- glm(count ~ nitrate, 
+    data = df_count,
+    family = "poisson")
+
+
+summary(m_pois)
